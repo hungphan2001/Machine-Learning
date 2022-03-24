e990e20a54b2452c3bb6a6395e7c0f08fdc2deb9
Hưng Phan <phandinhhungvp2001@gmail.com>
2022-03-24 11:40:33 +0700

    real estate:update
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- real estate.py
+++ real estate.py
@@ -1,71 +1,42 @@
-import pandas as pd 
-
-#Đọc file Real estate.csv vào  real_estate_df. 
-estate_data = pd.read_csv("Real estate.csv")
-# print(fashion_data.shape)
-
-# estate_data = estate_data.sample(frac = 1)
-print(estate_data)
-print(estate_data.columns)
-
-X = estate_data.drop(["No","Y house price of unit area"], axis = 1)
-print(X)
-y = estate_data["Y house price of unit area"]
-print(y)
-
-# Chia ngẫu nhiên X,y thành X_train,y_train và X_test,y_test theo tỉ lệ 50:50.
-#Sử dụng kỹ thuật hồi quy Linear Regression để huấn luyện với X_train,y_train.
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
-# # print(X_train.shape)
-
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split  
 from sklearn.linear_model import LinearRegression
-lr_model= LinearRegression().fit(X_train, y_train)
-print(lr_model.coef_)
+from sklearn import metrics
 
-y_pred = lr_model.predict(X_test)
+# 1 Đọc file Real estate.csv vào  real_estate_df. 
+real_estate_df = pd.read_csv('Real estate.csv')
+print(real_estate_df)
 
-# # print(y_pred)
-# # print(y_test)
-from sklearn.metrics import mean_squared_error
-print(mean_squared_error(y_test, y_pred))
-from sklearn.metrics import r2_score
-print(r2_score(y_test, y_pred))
+# 2 loại bỏ cột đầu tiên, gán y là cột cuối cùng và X là các cột còn lại. 
+real_estate_df = real_estate_df.drop(["No"],axis = 1)
+X = real_estate_df.drop(["Y house price of unit area"],axis = 1)
+y = real_estate_df["Y house price of unit area"]
 
-X5 = X.iloc[5,:]
-y5 = y.iloc[5]
-print(X5)
-print(y5)
-y_pred5 = lr_model.predict([X5])
-print(y_pred5)
-#tính mse 
-print("Bình phương sai số bằng hàm", mean_squared_error([y5],y_pred5))
-print("Bình phương sai số tính tay ",(y5-y_pred5)*(y5-y_pred5))
+# 3 Chia ngẫu nhiên X,y thành X_train,y_train và X_test,y_test theo tỉ lệ 50:50.
+# Sử dụng kỹ thuật hồi quy Linear Regression để huấn luyện với X_train,y_train.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+model_lr = LinearRegression().fit(X_train, y_train)
 
-# print(clf.predict([[2013.58300, 35.90000, 616.40040, 3.00000, 24.97723, 121.53767]]))
-# print(mean_squared_error([36.8], clf.predict([[2013.58300, 35.90000, 616.40040, 3.00000, 24.97723, 121.53767]])))
+# 4 Dự đoán trên tập X_test 
+y_pred = model_lr.predict(X_test)
+print(y_pred)
 
+# 5 Tính  mse 
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
 
-
-# from sklearn.neighbors import KNeighborsClassifier
-# neigh = KNeighborsClassifier(n_neighbors=2).fit(X_train, y_train)
-# y_pred = neigh.predict(X_test)
-# print(accuracy_score(y_test, y_pred))
-
-
-# from sklearn.cluster import KMeans
-# kmeans = KMeans(n_clusters=19).fit(X_train)
-
-# number = X.iloc[3,:].to_numpy().reshape((28,28))
-# print(number)
-
-# import cv2
-# import numpy as np
-# cv2.imshow("anh",number.astype(np.uint8))
-
-# cv2.waitKey(0) 
-  
-# #closing all open windows 
-# cv2.destroyAllWindows()
-
-
+# 6 (0.5đ). Dự đoán kết quả và đánh giá độ chính xác dự đoán trên một điểm dữ liệu (X57,y57) với:
+# X57= [2013.58300, 35.90000, 616.40040, 3.00000, 24.97723, 121.53767] và y57 = 36.8
+X57 = [2013.58300, 35.90000, 616.40040, 3.00000, 24.97723, 121.53767] 
+y57 = 36.8
+X57_new = pd.DataFrame([X57], 
+             columns=['X1 transaction date',  'X2 house age',  'X3 distance to the nearest MRT station',  'X4 number of convenience stores', 'X5 latitude', 'X6 longitude'])
+#y57_new =  pd.DataFrame([y57], columns=['Y house price of unit area'])
+#y_pred57 = model_lr.predict([X57])
+y_pred57 = model_lr.predict(X57_new)
+print(y_pred57)
+# print('Kiểm tra độ chính xác')
+# r2_score = model_lr.score(X57_new,y57_new)
+# r2_score = model_lr.score(X_test,y_test)
+# print("Độ chính xác của hồi quy tuyến tính: ",r2_score*100,'%')
